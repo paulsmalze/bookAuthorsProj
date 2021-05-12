9e24879ce6f0ed343d0b48b50595fae7d3daa400
paulsmalze <waffwegs@yahoo.com>
2021-05-12 18:29:18 -0500

    added an Author class model

--- booksAuthorsApp/models.py
+++ booksAuthorsApp/models.py
@@ -7,6 +7,12 @@ class Book(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 
 
 
